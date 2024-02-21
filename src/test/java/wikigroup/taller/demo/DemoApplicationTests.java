6279e75a2742dab729401eaa90a5d906dee651f4
Gabnegrin <gabrielandres.negrinh@gmail.com>
2024-02-20 19:51:04 -0500

    holaMundo

---
+++ src/test/java/wikigroup/taller/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wikigroup.taller.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
